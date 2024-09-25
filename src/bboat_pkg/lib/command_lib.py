d62f5b70873273227a99ef496bdaccf99a99513a
loickdegorre <loick.degorre@ensta-bretagne.fr>
2024-09-25 11:15:13 +0200

    cleaned and moved command lib

---
+++ src/bboat_pkg/lib/command_lib.py
@@ -0,0 +1,12 @@
+"""
+Author: Luiz Henrique Marques Gonçalves
+Creation Date: May - July 2024
+
+Description:
+This library was developed during my Projet de Recherche at ENSTA Bretagne from May to July 2024.
+It provides a series of functions and control laws used for analyzing various nonlinear control techniques for autonomous maritime systems.
+"""
+
+import numpy as np
+
+# INSERT COMMAND FUNCTIONS HERE
